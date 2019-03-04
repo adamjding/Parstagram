2b9f1ab45ade753b0c6aaeef96b2791fb7e8a3c5
Adam Ding <adamding@Adams-MacBook-Pro.local>
2019-03-04 16:15:59 -0500

    Finish part 2

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Adam Ding on 3/3/19.
+//  Copyright © 2019 Adam Ding. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
